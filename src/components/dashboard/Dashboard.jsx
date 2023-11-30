144b74740fad36cb23316597de9009dcc6a9c6b4
nadiaS11 <nadiasultana.ns11@gmail.com>
2023-11-30 23:38:28 +0600

    added new changes

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -12,26 +12,7 @@ const Dashboard = () => {
       <StyledEngineProvider injectFirst>
         <PersistentDrawerLeft />
       </StyledEngineProvider>
-      <div className="p-16">
-        <div className="p-8 bg-white shadow mt-24">
-          {" "}
-          <div className=" flex flex-col  ">
-            <div className="  relative ">
-              {" "}
-              <div className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
-                <img className="w-[60%]  " src={user?.photoURL} alt="" />
-              </div>{" "}
-            </div>{" "}
-          </div>{" "}
-          <div className="mt-32 text-center border-b pb-12">
-            {" "}
-            <h1 className="text-4xl font-medium text-gray-700">
-              {user?.displayName}
-            </h1>{" "}
-            <p className="font-light text-gray-600 mt-3">{user?.email}</p>{" "}
-          </div>{" "}
-        </div>
-      </div>
+
       <div className="pl-5  min-h-screen">
         <Outlet></Outlet>
       </div>
